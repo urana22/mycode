d7c1665c790bf22e63af334d0f9ff15fb5f368a1
urana22 <umarsalman@gmail.com>
2019-06-24 11:35:39 -0400

    your commot message1

---
+++ dict01.py
@@ -0,0 +1,12 @@
+#!/usr/bin/pythons3
+
+def main():
+    switch= {'hostname': 'sw1', 'ip': '10.0.1.1', 'version' : '1.2', 'vendor' : 'cisco'}
+
+    print( switch['hostname'])
+    print( switch['ip'])
+
+    switch['lynx'] = '1.1.1.1'
+    print( switch['lynx'])
+
+main()
